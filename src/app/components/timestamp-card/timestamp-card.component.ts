8e52f44c2d4e63119b2b61104b7b21d62cb374be
Ziga <ziga.koracin@gmail.com>
2024-09-05 13:38:41 +0200

    NOM8 Refresh data on click.

--- src/app/components/timestamp-card/timestamp-card.component.ts
+++ src/app/components/timestamp-card/timestamp-card.component.ts
@@ -1,15 +1,19 @@
 import { Component, inject } from '@angular/core';
 import { WeatherService } from '../../services/weather.service';
-
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-timestamp-card',
   standalone: true,
-  imports: [],
+  imports: [TranslateModule],
   templateUrl: './timestamp-card.component.html',
   styleUrl: './timestamp-card.component.scss'
 })
 export class TimestampCardComponent {
   weatherService = inject(WeatherService);
+
+  refreshData() {
+    this.weatherService.getWeatherForecast();
+  }
 
 }
