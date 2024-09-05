5038ed236194faacfe9df35f6164cff2f51bde89
Ziga <ziga.koracin@gmail.com>
2024-09-05 11:30:25 +0200

    NOM3 Added interfaces, fetching data with params, basic template display.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { WeatherService } from './services/weather.service';
 
 @Component({
   selector: 'app-root',
@@ -10,4 +11,9 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'weather-app';
+  weatherService = inject(WeatherService);
+
+  constructor() {
+    this.weatherService.getWeatherForecast();
+  }
 }
---
+++ src/app/services/weather.service.ts
@@ -0,0 +1,61 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { Forecast, List, WeatherApiResponse } from '../interfaces/weather.interface';
+import { map } from 'rxjs';
+import { environment } from '../../environments/environment';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WeatherService {
+  private http = inject(HttpClient);
+  private baseUrl = environment.baseUrl;
+  private apiKey = environment.apiKey;
+
+  forecastToday = signal<Forecast | null>(null);
+  forecastFuture = signal<Forecast[] | null>(null);
+
+  getWeatherForecast() {
+    const params = this.setUrlParams();
+    this.http.get<WeatherApiResponse>(this.baseUrl, {params}).pipe(
+      map(res => {
+        const city = res.city.name;
+        const list = res.list;
+        const groupedData = list.reduce((acc, item) => {
+          const date = item.dt_txt.split(' ')[0];
+          if (!acc[date]) {
+            acc[date] = [];
+          }
+          acc[date].push({
+            ...item
+          });
+          return acc;
+        }, {} as Record<string, List[]>);
+      
+        const formattedData = Object.entries(groupedData).map(([date, forecasts]) => ({
+          date: date,
+          dateForecasts: forecasts
+        }));
+      
+        return {
+          city,
+          forecasts: formattedData
+        };
+      })
+    ).subscribe(res => {
+      this.forecastToday.set(res.forecasts[0]);
+      this.forecastFuture.set(res.forecasts.slice(1));
+    });
+  }
+
+  private setUrlParams(lang='en', lat='46.5547', lon='15.6467', units='metric') {
+    let params = new HttpParams();
+    params = params.append('lat', lat);
+    params = params.append('lon', lon);
+    params = params.append('units', units);
+    params = params.append('lang', lang);
+    params = params.append('appid', this.apiKey);
+    return params;
+  }
+}
